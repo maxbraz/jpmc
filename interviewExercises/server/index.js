c8e025521bf27be00830f6c447d8888f046b9486
Max Braz <braz.max@gmail.com>
2017-09-04 23:51:13 -0700

    add parsing and geolocation to get path

--- interviewExercises/server/index.js
+++ interviewExercises/server/index.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const parser = require('parse-address');
+const zipcodes = require('zipcodes');
 const Banks = require('../db/index.js');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, './../client/dist')));
+
+app.post('/address', (req, res) => {
+  let parsedLocation = parser.parseLocation(req.body.address);
+  let detailedAddress = zipcodes.lookup(parsedLocation.zip);
+  let coordinates = {
+    latitude: detailedAddress.latitude,
+    longitude: detailedAddress.longitude,
+  };
+
+  res.send(JSON.stringify(coordinates));
+});
 
 app.get('/banks', (req, res) => {
   Banks.find({}).limit(5).exec( (err, banks) => {
