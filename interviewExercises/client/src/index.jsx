dc9e29449d4af3e51e576d55a1c067eb87ba9a0c
Max Braz <braz.max@gmail.com>
2017-09-04 23:50:33 -0700

    changes to get and post request

--- interviewExercises/client/src/index.jsx
+++ interviewExercises/client/src/index.jsx
@@ -10,18 +10,47 @@ class App extends React.Component {
     this.state = {
       banks: []
     }
-
+    this.fetch = this.fetch.bind(this);
     this.search = this.search.bind(this);
   }
 
-  search (term) {
+  search(address) {
+    let coordinates;
+    axios.post('/address', {
+      address: address
+    })
+    .then(function(response) {
+      coordinates = response.data;
+    })
+    .catch(function(error) {
+      console.log(error);
+    })
+
+    this.fetch(coordinates);
+  }
+
+  fetch(coordinates) {
+    let fetchedBanks;
+
+    axios.get('/banks', {
+      params: {
+        coordinates: coordinates
+      }
+    })
+    .then(function (response) {
+      fetchedBanks = response.data;
+      this.setState({banks: fetchedBanks})
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
   }
 
   render () {
     return (<div>
       <h1>JPMC Bank Search </h1>
-      <BankList banks={this.state.banks}/>
       <Search onSearch={this.search}/>
+      <BankList banks={this.state.banks}/>
     </div>)
   }
 }
