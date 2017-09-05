b7923f6470455e8e2f26eb830f28293ccf523dd3
Max Braz <braz.max@gmail.com>
2017-09-05 16:06:09 -0700

    update query

--- interviewExercises/server/index.js
+++ interviewExercises/server/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const parser = require('parse-address');
 const zipcodes = require('zipcodes');
-const Banks = require('../db/index.js');
+const Bank = require('../db/index.js');
 const findLocation = require('./middleware/findLocation.js');
 
 const app = express();
@@ -22,35 +22,38 @@ app.post('/address', (req, res) => {
 });
 
 app.get('/banks', (req, res) => {
-  let limit = req.query.limit || 5;
+  console.log('this is the req.query: ', req.query);
 
-  let maxDistance = req.query.distance || 7;
-  maxDistance /= 6371;
+  // let maxDistance = 7;
+  // maxDistance /= 6371;
 
   let coordinates = [];
-  coordinates[0] = req.query.longitude || 0;
-  coords[1] = req.query.latitude || 0;
+  coordinates[0] = req.query.longitude || -82.997928;
+  coordinates[1] = req.query.latitude || 40.151287;
 
-  Bank.find({
-    loc: {
-      $near: coordinates,
-      $maxDistance: maxDistance,
-    }
-  }).limit(limit).exec(function(err, locations) {
-    if (err) {
-      return res.json(500, err);
-    }
-
-    res.json(200, locations);
-  });
-  // Banks.find({}).limit(5).exec( (err, banks) => {
+  // Bank.find({
+  //   loc: {
+  //     $near: {
+  //       $geometry: {
+  //         type: 'Point',
+  //         coordinates: coordinates,
+  //       },
+  //     }
+  //   }
+  // }).limit(5).exec(function(err, banks) {
   //   if (err) {
-  //     console.log( 'server get request failure', err);
-  //   } else {
-  //     console.log('Success!');
+  //     console.log(err);
   //   }
   //   res.end(JSON.stringify(banks));
   // });
+  Bank.find({}).limit(5).exec( (err, banks) => {
+    if (err) {
+      console.log( 'server get request failure', err);
+    } else {
+      console.log('Success!');
+    }
+    res.end(JSON.stringify(banks));
+  });
 });
 
 const port = process.env.PORT || 5000;
