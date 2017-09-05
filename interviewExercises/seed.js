238e4434d6397ff06e1a10f5f5290320d3387378
Max Braz <braz.max@gmail.com>
2017-09-04 19:07:20 -0700

    add seed file to seed db with fake bank data

---
+++ interviewExercises/seed.js
@@ -0,0 +1,22 @@
+let Bank = require('./db/index.js');
+let banks = require('./data.json');
+
+for (let bank of banks) {
+  let banko = new Bank({
+    name: bank.name,
+    street: bank.street,
+    city: bank.city,
+    state: bank.state,
+    zipcode: bank.zipcode,
+    latitude: bank.latitude,
+    longitude: bank.longitude,
+  });
+  banko.save((err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('successfully seeded bank');
+    }
+  });
+  counter++;
+}
